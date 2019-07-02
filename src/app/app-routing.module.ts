c793ff58757e2c74b70f5c4159ed15fe9d1e2a63
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-02 17:41:45 +0530

    generate intro, checklist and dataservice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' },
+  { path: 'checklist', loadChildren: './checklist/checklist.module#ChecklistPageModule' },
 ];
 
 @NgModule({
