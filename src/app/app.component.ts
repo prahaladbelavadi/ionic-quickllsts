22af26873f334504951565b55f6271f4e0fb9ab8
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-02 18:13:32 +0530

    use capacitor components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,24 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+const { SplashScreen, StatusBar } = Plugins;
+
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
+  constructor() {
+    SplashScreen.hide().catch((err) => {
+      console.warn(err);
+    });
+
+    StatusBar.hide().catch((err) => {
+      console.warn(err);
+    });
+
   }
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
-  }
 }
