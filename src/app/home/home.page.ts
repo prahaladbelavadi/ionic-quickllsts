4f37c762e558859298d6553f13ce3e159dc3beb0
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-03 19:39:41 +0530

    add addchecklist function
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from "@ionic/angular";
+import { ChecklistDataService } from "../services/checklist-data.service";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public dataService: ChecklistDataService, private alertCtrl: AlertController) { }
+
+  addChecklist(): void {
+    this.alertCtrl.create({
+      header: 'New Checklist',
+      message: 'Enter the name of your new checklist below:',
+      inputs: [
+        {
+          type: 'text',
+          name: 'name'
+        }
+      ], buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: (data) => {
+            this.dataService.createChecklist(data);
+          }
+        }
+      ]
+    }).then((prompt)=>{
+      prompt.present();
+    });
+  }
+
+  renameChecklist(checklist): void {
+
+  }
+
+  removeChecklist(checklist): void {
+
+  }
 
 }
